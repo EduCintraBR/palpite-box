c4ce173bc9712235fcae3ec0409873d347ad05f0
Eduardo Cintra <educin15@hotmail.com>
2020-07-07 22:03:45 -0300

    Add contact page

--- components/Footer.js
+++ components/Footer.js
@@ -3,22 +3,8 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <footer className="bg-footer py-4 text-center">
-            <span className="text-white font-bold">Projeto desenvolvido por Eduardo Cintra</span>
-            <div className="group-contact">
-                <ul>
-                    <li>
-                        <a href="https://www.linkedin.com/in/educintrabr/">
-                            <FaLinkedin size={32} />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/EduCintraBR">
-                            <FaGithub size={32} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
+        <footer className="bg-footer py-8 text-center">
+            <span className="text-white font-bold">Este site foi desenvolvido com NextJS + Vercel (como plataforma)</span>
         </footer>
     )
 }
--- pages/contato.js
+++ pages/contato.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import Link from 'next/link';
+import { FaLinkedin, FaGithub, FaLaptop } from 'react-icons/fa'
 
 
 const Contato = () => {
     return (
-        <div>
-            <h1>Contato</h1>
-            <div>
-                <Link href="/">
-                    <a>Voltar</a>
-                </Link>
+        <div className="py-16">
+            <h1 className="text-center text-4xl text-green-800">Olá, eu sou o Eduardo Cintra</h1>
+            <p className="text-md text-center">Gostaria de me conhecer mais? Entre em contato comigo!</p>
+            <div className="group-contact pt-8">
+                <ul>
+                    <li>
+                        <a href="https://www.linkedin.com/in/educintrabr/">
+                            <FaLinkedin size={64} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/EduCintraBR">
+                            <FaGithub size={64} />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://eduardocintra.dev">
+                            <FaLaptop size={64} />
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     )
